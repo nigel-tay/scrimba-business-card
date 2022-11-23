9026f11ff3f8f05c5e7def73901b641e00ae632a
nigel-tay <ninenigel@gmail.com>
2022-11-23 18:15:24 +0800

    Add footer social icons with styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { FaTwitterSquare, FaFacebookSquare, FaInstagramSquare, FaGithubSquare } from 'react-icons/fa';
 
 export default function Footer() {
     return (
-        <div>
+        <div className="footer-container background-class">
+            <FaTwitterSquare className="social-icons" />
+            <FaFacebookSquare className="social-icons" />
+            <FaInstagramSquare className="social-icons" />
+            <FaGithubSquare className="social-icons" />
         </div>
     )
 }
