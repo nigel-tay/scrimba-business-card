510b686c5dc293c263d3a8f0d9474ba5aa091a17
nigel-tay <ninenigel@gmail.com>
2022-11-23 17:48:08 +0800

    Add About and Interests section with styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 export default function About() {
     return (
-        <div>
+        <div className="about-container about-interests background-class">
+            <h1>About</h1>
+            <p>
+                I am a frontend developer with a particular interest in making things simple 
+                and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
+            </p>
         </div>
     )
 }
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 export default function Interests() {
     return (
-        <div>
+        <div className="interests-container about-interests background-class">
+            <h1>Interests</h1>
+            <p>
+            Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. 
+            Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+            </p>
         </div>
     )
 }
