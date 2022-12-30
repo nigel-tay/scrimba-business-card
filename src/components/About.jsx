9a66fb4ee0c805c74fd210fa4869524ec4059b98
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2022-12-30 16:34:19 +0800

    feat: Add tailwind classes to About and Interests components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function About() {
     return (
-        <div className="about-container about-interests bg-background-black">
-            <h1>About</h1>
-            <p>
+        <div className="p-5 pb-1 bg-background-black">
+            <h1 className="pb-2 font-bold text-base">About</h1>
+            <p className="font-normal text-xs">
                 I am a frontend developer with a particular interest in making things simple 
                 and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
             </p>
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Interests() {
     return (
-        <div className="interests-container about-interests bg-background-black">
-            <h1>Interests</h1>
-            <p>
+        <div className="pt-5 pb-8 p-5 bg-background-black">
+            <h1 className="pb-2 font-bold text-base">Interests</h1>
+            <p className="font-normal text-xs">
             Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. 
             Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
             </p>
